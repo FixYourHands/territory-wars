6f94b4acca9741ed72151e6c684a5a29b3eea83f
Azjae <azjaewashington@gmail.com>
2025-10-22 18:28:37 -0700

    Added visual studio files and main.cpp to git tracking

--- TWGame/main.cpp
+++ TWGame/main.cpp
@@ -1,27 +1,49 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "mouseEventProcessor.h"
 
 
 using Circle = sf::CircleShape;
+using Rectangle = sf::RectangleShape;
 using Event = sf::Event;
+
+
+enum class ShapeType
+{
+	Circle, Rectangle
+};
+
+
 int main()
 {
-	sf::RenderWindow window{ sf::VideoMode({ 400,400 }), "My SFML Window" };
+	sf::RenderWindow window{ sf::VideoMode({ 800,800 }), "My SFML Window" };
+	sf::Texture texture("images/boy_with_bag.png");
+	sf::Sprite sprite(texture);
 
-	Circle circle1{ 200.f };
+	sf::Vector2u vec{ window.getSize() };
+	auto [width, height] {vec};
+	
+	Circle circle1{ 100.f };
+	circle1.setFillColor(sf::Color::Green);
 
 	while (window.isOpen())
 	{
 		while (const std::optional event = window.pollEvent())
 		{
-			if (event->is<Event>())
+			if (event->is<Event::Closed>())
+			{
 				window.close();
-		}
-	}
+			}
 
-	window.clear();
-	window.draw(circle1);
-	window.display();
+			MouseEventProcessor MEP(event);
+				
+		}
+
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
+	
 
 
 }
